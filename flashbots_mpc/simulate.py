54479980c9c09544a737361155b90a5fa975f293
Mikerah <mikerah@hashcloak.com>
2022-01-12 13:45:52 -0500

    Modified simulate.py to create a public shared mempool for bundles

--- flashbots_mpc/simulate.py
+++ flashbots_mpc/simulate.py
@@ -10,21 +10,22 @@ def generate_random_tx():
     priority_fee = random.uniform(0, 3000001)
     return flashbots_types.Tx(gas, gas_price, fee_cap, priority_fee)
 
+mempool_txs = []
+mempool_txs.extend([generate_random_tx() for i in range(100)])
+
 for i in range(10):
     with  open("Player-Data/Input-P{}-0".format(i), 'w+') as f:
         txs = []
-        mempool_txs = []
-        for j in range(3):
+        for j in range(6):
             tx = generate_random_tx()
             txs.append(tx)
 
         bundle = flashbots_types.Bundle(txs)
-        mempool_txs.extend(random.sample(txs + [generate_random_tx() for i in range(10)], 6))
+        mempool_txs.extend(random.sample(txs, 3))
         coinbase_difference = random.uniform(0, 3000001)
         basefee = random.uniform(0, 3000001)
-        f.write("bundle: \n" + str(bundle) + "\n")
-        f.write("mempool_txs: \n" + "\n".join([str(tx) for tx in mempool_txs]) + "\n")
-        f.write("coinbase_difference: " + str(coinbase_difference) + "\n")
-        f.write("basefee: " + str(basefee) + "\n")
-        
-    
+        f.write(str(bundle) + "\n")
+        f.write(str(coinbase_difference) + "\n")
+        f.write(str(basefee) + "\n")
+with open("../MP-SPDZ/Programs/Public-Input/bundle_scoring", "w+") as m:
+    m.write("\n".join([str(tx) for tx in mempool_txs]))
