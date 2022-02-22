00c861e7ae1d969980a71947d385d3147ec123bb
Mikerah <mikerah@hashcloak.com>
2022-02-22 00:43:57 -0500

    More debugging + removal of unecessary imports, comments, etc

--- flashbots_mpc/flashbots_types.py
+++ flashbots_mpc/flashbots_types.py
@@ -2,7 +2,6 @@ import sys
 
 # Get MP-SPDZ types
 sys.path.append("../MP-SPDZ/")
-from Compiler.types import cfix
 
 class Tx:
     def __init__(self, gas, gas_price, fee_cap, priority_fee):
@@ -15,8 +14,8 @@ class Tx:
         return min(self.fee_cap, basefee + self.priority_fee)
 
     def calculate_miner_fee_per_gas(self, basefee):
-        effecitve_fee = self.calculate_effective_fee_per_gas(basefee)
-        return effecitve_fee - basefee
+        effective_fee = self.calculate_effective_fee_per_gas(basefee)
+        return effective_fee - basefee
 
     def to_int_rep(self, f=16):
         gas = round(self.gas * (2**f))
